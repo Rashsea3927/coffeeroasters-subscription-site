578796bf0c1d085f8404b486e83be58c96d62bc5
Rashsea <kaji.dev3927@gmail.com>
2025-11-19 18:18:39 +0900

    feat: トップページにサブスクリプションサイトの紹介テキストを追加

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
       <h1>Hello World</h1>
+      <p>Welcome to the Coffee Roasters Subscription Site</p>
     </div>
   );
 }
